a6a70603d7140cab939cc599f5cb21fb26a5b16c
Mariela-t <tinoco.mariela01@gmail.com>
2023-11-28 21:00:45 -0800

    Route for New Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :posts
   get '/pages', to: 'pages#index'
+   get 'pages/new', to: 'pages#new'
   get '/pages/:id', to: 'pages#show', as: 'page'
+ 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
